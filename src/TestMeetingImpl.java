e4b37cfff988bd203db173be2252f25e7c1a0e08
sssteven <sssteven@hotmail.co.uk>
2016-03-13 09:32:27 +0000

    test meeting completed

--- src/TestMeetingImpl.java
+++ src/TestMeetingImpl.java
@@ -53,4 +53,45 @@ public class TestMeetingImpl {
         newContacts = null;
         Meeting newM = new ConcreteMeetingImpl(3,cal,newContacts);
     }
+
+    //Testing methods
+
+    @Test
+    public void testGetId() {
+        int input = 5;
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 3, 20);
+        newContacts.add(new ContactImpl(3, "Adam", "late"));
+        Meeting newM = new ConcreteMeetingImpl(input, cal, newContacts);
+        int output = newM.getId();
+        assertEquals(input, output);
+    }
+
+    @Test
+    public void testDate() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 3, 20);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(2016, 3, 21);
+        Calendar input = cal;
+        newContacts.add(new ContactImpl(3, "Adam", "late"));
+        Meeting newM = new ConcreteMeetingImpl(5, cal, newContacts);
+        Calendar output = newM.getDate();
+        assertEquals(input, output);
+    }
+
+    @Test
+    public void testContacts() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 3, 20);
+        newContacts.add(new ContactImpl(3, "Adam", "late"));
+        newContacts.add(new ContactImpl(4, "Bob", "late"));
+        Set<Contact> newContacts2 = new HashSet<Contact>();
+        newContacts2.add(new ContactImpl(3, "Adam", "late"));
+        Set<Contact> input = newContacts;
+        Meeting newM = new ConcreteMeetingImpl(5, cal, newContacts);
+        Set<Contact> output = newM.getContacts();
+        assertEquals(input, output);
+    }
+
 }
